adbe3ff05a8ffd9908d328b89c474eb0d101d83f
mukukuu <qinii@me.com>
2022-01-23 15:10:36 -0700

    added html routee

---
+++ db/nootebook.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+//use util module
+const util = require('util');
+
+//use uuid to generate id
+const uuid = require("uuid").v4;
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,32 @@
 const router = require('express').Router();
 
+const notebook = require('../db/db.json');
+
+
+
+
+//request notebook
+router.get('./notes', (req, res) => {
+    notebook.getnotes() 
+    .then(notes => {res.json(notes)})
+    .catch(err => {res.status(500).json(err)})
+});
+
+//response 
+router.post('/notes', (req, res) => {
+    notebook
+    .addNote(req.body)
+    .then(note => {res.json(note)})
+    .catch(err => {res.status(500).json(err)})
+})
+
+//delete note
+router.delete('/notes/:id', (req, res) => {
+    nootbook
+    .deleteNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch(err => res.status(500).json(err))
+})
+
+
+module.exports = router;
